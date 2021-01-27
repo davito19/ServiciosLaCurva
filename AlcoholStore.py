37db1d57dabc24e9fb7474840b7602182743f78e
Davito19 <omardbonett@gmail.com>
2021-01-27 17:38:32 -0500

    actualizado

--- AlcoholStore.py
+++ AlcoholStore.py
@@ -1,63 +1,7 @@
 from socketserver import ThreadingTCPServer, BaseRequestHandler
-import objetos as ob
-import sqlite3 as sql
+from base_datos import alco
+from base_datos import clientesA as clientes
 
-
-class BaseDatos:
-    def __init__(self, base_datos : str, table : str):
-        self.base_datos = base_datos
-        self.conexion = sql.connect(self.base_datos, check_same_thread=False)
-        self.cursor = self.conexion.cursor()
-    def __str__(self):
-        return "Base de datos de {}".format(self.base_datos)
-
-class DataCliente(BaseDatos):
-    def __init__(self, base_datos : str, table : str):
-        super().__init__(base_datos, table)
-        self.cursor.execute("SELECT * FROM " + table)
-        self.data = self.cursor.fetchall()
-        self.clientes = {}
-        for i in self.data:
-            moves = i[5].split(";")
-            self.clientes[i[0]] = ob.ClienteAlcoholeria(i[0],i[1],i[2],i[3],i[4],moves)
-    def search_id(self, id):
-        for key in self.clientes:
-            if id == self.clientes[key].get_id():
-                return True
-        return False
-    def search_password(self, id, password):
-        if self.clientes[id].password_validation(password):
-            return True
-        else:
-            return False
-    def get_data(self):
-        return self.data
-
-class DataAlcohol(BaseDatos):
-    def __init__(self, base_datos : str, table : str):
-        super().__init__(base_datos, table)
-        self.cursor.execute("SELECT * FROM " + table)
-        self.data = self.cursor.fetchall()
-        self.alcoholes = {}
-        for i in self.data:
-            self.alcoholes[i[0]] = ob.Alcohol(i[0], i[1], i[2], i[3], i[4])
-    def listar_alcoholes(self):
-        string = ""
-        for key in self.alcoholes:
-            string += str(self.alcoholes[key])
-        return string
-    def comprar_alcoholes(self, id, cantidad):
-        temp = self.alcoholes[id].compra(cantidad)
-        self.refresh(id)
-        return temp
-    def refresh(self, id):
-        sql = "UPDATE ALCOHOLES SET Unidades = '"+str(self.alcoholes[id].get_stock())+ "' where ID = '" + str(id) +"'"
-        self.cursor.execute(sql)
-        self.conexion.commit()
-
-
-clientes =  DataCliente("clientes_licoreria", "Clientes")
-alco = DataAlcohol("Alcoholes", "ALCOHOLES")
 users = 0
 
 class QueHacer(BaseRequestHandler):
