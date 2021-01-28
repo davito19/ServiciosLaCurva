e4dcc7d129febe2ea2f5082e599330f15873fe63
Davito19 <omardbonett@gmail.com>
2021-01-27 22:55:28 -0500

    Agregando el paraetro ip para conexion udp

--- alcohlesserveudp.py
+++ alcohlesserveudp.py
@@ -1,10 +1,9 @@
 import socket as sk
 
-
-def udpconexion(cuennta, cantidad):
+def udpconexion(cuennta, cantidad, ip):
     string = str(cuennta)+" ; "+ str(cantidad)
     c = sk.socket(sk.AF_INET,sk.SOCK_DGRAM)
-    c.sendto(string.encode(),("localhost",6689))
+    c.sendto(string.encode(),(ip, 6689))
     data, remote = c.recvfrom(1024)
     print(data.decode())
     data, remote = c.recvfrom(1024)
